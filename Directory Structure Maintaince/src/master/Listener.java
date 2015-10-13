700e9912013800304857c03ab90f1a5adbf13eb4
jayakarthigayan <jayakarthigayan@gmail.com>
2015-10-12 19:41:37 -0500

    Added the lister changes for the MDS servers.

--- Directory Structure Maintaince/src/master/Listener.java
+++ Directory Structure Maintaince/src/master/Listener.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
  */
 public class Listener implements Runnable {
 	public volatile boolean			isRunning			= true;
+	private String 					listenerType;
 	private final ServerSocket		listenerSocket;
 	private final ArrayList<Thread>	workerThreadPool	= new ArrayList<Thread>();
 
@@ -25,8 +26,9 @@ public class Listener implements Runnable {
 	 * @param socket
 	 *            Socket to listen on
 	 */
-	public Listener(final ServerSocket socket) {
+	public Listener(final ServerSocket socket, final String listenerType) {
 		listenerSocket = socket;
+		this.listenerType = listenerType;
 	}
 
 	/*
@@ -46,7 +48,7 @@ public class Listener implements Runnable {
 				workerThread.start();
 			} catch (final Exception e) {
 				e.printStackTrace();
-			}
+			}			
 		}
 
 		// Wait for all worker threads to finish
