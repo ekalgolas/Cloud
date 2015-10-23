0f128aee1770379a994d0479760c462788ce35c9
ketanjoshi <ketanshine777@gmail.com>
2015-10-23 17:37:50 -0500

    Preventing file or folder creation if already present

--- Directory Structure Maintaince/src/master/gfs/GFSDirectoryOperations.java
+++ Directory Structure Maintaince/src/master/gfs/GFSDirectoryOperations.java
@@ -7,7 +7,6 @@ import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 
 import com.sun.media.sound.InvalidDataException;
-
 import commons.CompletionStatusCode;
 import commons.Message;
 import commons.OutputFormatter;
@@ -239,16 +238,21 @@ public class GFSDirectoryOperations implements ICommandOperations {
 			throw new InvalidPathException(path, "Path was not found");
 		}
 
+		List<Directory> contents = directory.getChildren();
+		for (Directory child : contents) {
+			if(child.getName().equalsIgnoreCase(name)) {
+				throw new InvalidPathException(path, "Path already present");
+			}
+		}
+
 		// Add file if isFile is true
 		if (isFile) {
 			final Directory file = new Directory(name, isFile, null);
-			directory.getChildren()
-			.add(file);
+			contents.add(file);
 		} else {
 			// Else, add directory here
 			final Directory dir = new Directory(name, isFile, new ArrayList<Directory>());
-			directory.getChildren()
-			.add(dir);
+			contents.add(dir);
 		}
 	}
 
