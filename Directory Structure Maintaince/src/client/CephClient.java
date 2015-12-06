0d6d47d1bb5d427fb8980584af7df768f352f39d
ketanjoshi <ketanshine777@gmail.com>
2015-12-06 17:35:07 -0600

    Avoid parsing input - ceph client

--- Directory Structure Maintaince/src/client/CephClient.java
+++ Directory Structure Maintaince/src/client/CephClient.java
@@ -414,7 +414,7 @@ public class CephClient {
 		final CommandGenerator generator = new CommandGenerator();
 		try {
 			generator.generateCommands(Integer.parseInt(AppConfig.getValue("client.numberOfCommands")));
-		} catch (NumberFormatException | IOException e) {
+		} catch (NumberFormatException | IOException | ClassNotFoundException e) {
 			// Exit if commands cannot be generated
 			LOGGER.error("Error occured while generating the commands", e);
 			System.exit(0);
